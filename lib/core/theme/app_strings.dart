13ea8bf36b386a00b8a907a17f10af2c90da1d58
NesterovOI <110261930+NesterovOI@users.noreply.github.com>
2026-07-28 21:47:36 +0300

    Add app_strings and firebase_auth_remote_data_source.dart

--- lib/core/theme/app_strings.dart
+++ lib/core/theme/app_strings.dart
@@ -1,21 +1,30 @@
 
 abstract class AppStrings {
-  // === ЗАГАЛЬНІ ЕЛЕМЕНТИ ===
+  //  ЗАГАЛЬНІ ЕЛЕМЕНТИ
   static const String appTitle = 'НМТ Експрес';
   static const String continueButton = 'ПРОДОВЖИТИ';
   static const String checkButton = 'ПЕРЕВІРИТИ';
   static const String nextLesson = 'НАСТУПНИЙ УРОК';
 
-  // === ТЕСТИ ТА ГРА ===
+  //  ТЕСТИ ТА ГРА
   static const String correctAnswers = 'Чудово! Правильно';
   static const String wrongAnswer = 'Неправильно. Правильна відповідь:';
   static const String outOfLivesTitle = 'Ой, життя закінчилися!';
   static const String outOfLivesSub = 'Зачекай відновлення або подивись відео, щоб отримати +1 ❤️';
   static const String watchAdButton = 'Отримати життя за рекламу';
 
-  // === НИЖНЯ ПАНЕЛЬ (Bottom Nav) ===
+  //  НИЖНЯ ПАНЕЛЬ (Bottom Nav)
   static const String navHome = 'Головна';
   static const String navDictionary = 'Словник';
   static const String navLeaderboard = 'Рейтинг';
   static const String navProfile = 'Профіль';
+
+  // ОБРОБКА ПОМИЛОК АВТОРИЗАЦІЇ
+  static const String weakPassword = 'Пароль занадто простий (мінімум 6 символів).';
+  static const String emailAlreadyInUse = 'Акаунт з цим Email вже існує.';
+  static const String invalidEmail = 'Некоректна електронна адреса.';
+  static const String invalidEmailOrPassword = 'Неправильний Email або пароль.';
+  static const String invalidAuth = 'Помилка авторизації.';
+  static const String invalidCreateUser = 'Не вдалося створити користувача.';
+  static const String userNotFound = 'Користувача не знайдено.';
 }
--- lib/features/auth/data/datasources/firebase_auth_remote_data_source.dart
+++ lib/features/auth/data/datasources/firebase_auth_remote_data_source.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart' as firebase_auth;
 import 'package:ukrainian/features/auth/data/models/auth_model.dart';
+import 'package:ukrainian/core/theme/theme.dart';
 
 abstract class AuthRemoteDataSource {
   Future<AuthModel> signUpWithEmail({
@@ -18,33 +19,91 @@ abstract class AuthRemoteDataSource {
   Stream<AuthModel?> get authStateChange;
 }
 
-class FirebaseAuthRemoteDataSource implements AuthRemoteDataSource {
-  @override
-  // TODO: implement authStateChange
-  Stream<AuthModel?> get authStateChange => throw UnimplementedError();
+class FirebaseAuthRemoteDataSourceImpl implements AuthRemoteDataSource {
+  final firebase_auth.FirebaseAuth _firebaseAuth;
 
-  @override
-  Future<AuthModel> signInWithEmail({
-    required String email,
-    required String password,
-  }) {
-    // TODO: implement signInWithEmail
-    throw UnimplementedError();
-  }
-
-  @override
-  Future<void> signOut() {
-    // TODO: implement signOut
-    throw UnimplementedError();
-  }
+  FirebaseAuthRemoteDataSourceImpl({firebase_auth.FirebaseAuth? firebaseAuth})
+      : _firebaseAuth = firebaseAuth ?? firebase_auth.FirebaseAuth.instance;
 
   @override
   Future<AuthModel> signUpWithEmail({
     required String name,
     required String email,
     required String password,
-  }) {
-    // TODO: implement signUpWithEmail
-    throw UnimplementedError();
+  }) async {
+    try {
+      final userCredential = await _firebaseAuth.createUserWithEmailAndPassword(
+        email: email.trim(),
+        password: password.trim(),
+      );
+
+      final user = userCredential.user;
+      if (user == null) {
+        throw Exception(AppStrings.invalidCreateUser);
+      }
+
+      await user.updateDisplayName(name.trim());
+      await user.reload();
+
+      final updatedUser = _firebaseAuth.currentUser ?? user;
+
+      return AuthModel.fromFirebaseUser(updatedUser);
+
+    } on firebase_auth.FirebaseAuthException catch (e) {
+      throw _handleFirebaseAuthException(e);
+    }
+
+  }
+
+  @override
+  Future<AuthModel> signInWithEmail({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      final userCredential = await _firebaseAuth.signInWithEmailAndPassword(
+          email: email.trim(),
+          password: password.trim(),
+      );
+
+      final user = userCredential.user;
+      if (user == null) {
+        throw Exception(AppStrings.userNotFound);
+      }
+
+      return AuthModel.fromFirebaseUser(user);
+    } on firebase_auth.FirebaseAuthException catch (e) {
+      throw _handleFirebaseAuthException(e);
+    }
+  }
+
+  @override
+  Future<void> signOut() async{
+    await _firebaseAuth.signOut();
+  }
+
+  @override
+  Stream<AuthModel?> get authStateChange {
+    return _firebaseAuth.authStateChanges().map((firebaseUser){
+      if (firebaseUser == null) return null;
+      return AuthModel.fromFirebaseUser(firebaseUser);
+    });
+  }
+
+  Exception _handleFirebaseAuthException(firebase_auth.FirebaseAuthException e) {
+    switch (e.code) {
+      case 'weak-password':
+        return Exception(AppStrings.weakPassword);
+      case 'email-already-in_use':
+        return Exception(AppStrings.emailAlreadyInUse);
+      case 'invalid-email':
+        return Exception(AppStrings.invalidEmail);
+      case 'user-not-found':
+      case 'wrong-password':
+      case 'invalid-credential':
+        return Exception(AppStrings.invalidEmailOrPassword);
+      default:
+        return Exception(e.message ?? AppStrings.invalidAuth);
+    }
   }
 }
