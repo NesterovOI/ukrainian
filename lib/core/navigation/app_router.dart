9bb1cf6c40f2094ef15a710b362df12dc15abc72
NesterovOI <110261930+NesterovOI@users.noreply.github.com>
2026-07-29 19:49:18 +0300

    Made a change to app_router.dart user registration verification

--- lib/core/navigation/app_router.dart
+++ lib/core/navigation/app_router.dart
@@ -1,5 +1,7 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:ukrainian/features/auth/presentation/pages/pages.dart';
+import 'package:ukrainian/features/auth/presentation/provider/auth_state_provider.dart';
 import 'package:ukrainian/features/dictionary/presentation/pages/dictionary_page.dart';
 import 'package:ukrainian/features/home_lessons/presentation/pages/pages.dart';
 import 'package:ukrainian/features/leaderboard/presentation/pages/leaderboard_page.dart';
@@ -16,61 +18,86 @@ class AppRouter {
   static const String mainPage = '/main_page';
   static const String profilePage = '/profile_page';
 
-  static final GoRouter router = GoRouter(
-    initialLocation: splash,
+  final routerProvider = Provider<GoRouter>((ref) {
+    final authState = ref.watch(authStateProvider);
+    return GoRouter(
+      initialLocation: splash,
       routes: [
+        GoRoute(path: splash, builder: (context, state) => const SplashPage()),
         GoRoute(
-            path: splash,
-            builder: (context, state) => const SplashPage(),
+          path: createPassword,
+          builder: (context, state) => const CreatePasswordPage(),
         ),
         GoRoute(
-            path: createPassword,
-            builder: (context, state) => const CreatePasswordPage(),
-        ),
-        GoRoute(
-            path: loginPage,
-            builder: (context, state) => const LoginPage(),
+          path: loginPage,
+          builder: (context, state) => const LoginPage(),
         ),
 
         StatefulShellRoute.indexedStack(
-            builder: (context, state, navigationShell) {
-              return MainPage(navigationShell: navigationShell,);
-            },
-            branches: [
-              StatefulShellBranch(
-                  routes: [
-                    GoRoute(
-                        path: homePage,
-                        builder: (context, state) => const HomePage(),
-                    ),
-                  ],
-              ),
-              StatefulShellBranch(
-                  routes: [
-                    GoRoute(
-                        path: dictionary,
-                        builder: (context, state) => const DictionaryPage(),
-                    ),
-                  ],
-              ),
-              StatefulShellBranch(
-                  routes: [
-                    GoRoute(
-                        path: leaderboard,
-                        builder: (context, state) => const LeaderboardPage(),
-                    ),
-                  ],
-              ),
-              StatefulShellBranch(
-                  routes: [
-                    GoRoute(
-                        path: profilePage,
-                        builder: (context, state) => const ProfilePage(),
-                    ),
-                  ],
-              ),
-            ],
-        )
+          builder: (context, state, navigationShell) {
+            return MainPage(navigationShell: navigationShell);
+          },
+          branches: [
+            StatefulShellBranch(
+              routes: [
+                GoRoute(
+                  path: homePage,
+                  builder: (context, state) => const HomePage(),
+                ),
+              ],
+            ),
+            StatefulShellBranch(
+              routes: [
+                GoRoute(
+                  path: dictionary,
+                  builder: (context, state) => const DictionaryPage(),
+                ),
+              ],
+            ),
+            StatefulShellBranch(
+              routes: [
+                GoRoute(
+                  path: leaderboard,
+                  builder: (context, state) => const LeaderboardPage(),
+                ),
+              ],
+            ),
+            StatefulShellBranch(
+              routes: [
+                GoRoute(
+                  path: profilePage,
+                  builder: (context, state) => const ProfilePage(),
+                ),
+              ],
+            ),
+          ],
+        ),
       ],
-  );
+      redirect: (context, state) {
+        return authState.when(
+          data: (user) {
+            final isAuthPage =
+                state.matchedLocation == AppRouter.loginPage ||
+                state.matchedLocation == AppRouter.createPassword;
+            // 1. Якщо користувач АВТОРИЗОВАНИЙ і знаходиться на сторінці
+            // входу/реєстрації
+            // -> Відправляємо його на головну сторінку
+            if (user != null && isAuthPage) {
+              return AppRouter.homePage;
+            }
+            // 2. Якщо користувач НЕ АВТОРИЗОВАНИЙ і намагається зайти на закриті екрани
+            // -> Відправляємо його на сторінку входу
+            if (user == null &&
+                !isAuthPage &&
+                state.matchedLocation != AppRouter.splash) {
+              return AppRouter.loginPage;
+            }
+            return null;
+          },
+          loading: () => null,
+          error: (_, __) => AppRouter.loginPage,
+        );
+      },
+    );
+  });
 }
