9b6f8944741fd4adcd4bbffe37c603b32b9b5004
NesterovOI <110261930+NesterovOI@users.noreply.github.com>
2026-07-30 23:19:34 +0300

    Added user_name_provider.dart function setUserName for add user and function clearUserName for delete user

---
+++ lib/features/auth/presentation/provider/user_name_provider.dart
@@ -0,0 +1,26 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:ukrainian/core/prorvider/shared_preferences_provider.dart';
+
+const String kUserNameKey = 'user_name';
+final userNameProvider = NotifierProvider<UserNameNotifier, String>(UserNameNotifier.new);
+
+class UserNameNotifier extends Notifier<String> {
+  @override
+  String build() {
+    final prefs = ref.watch(sharedPreferencesProvider);
+    return prefs.getString(kUserNameKey) ?? '';
+  }
+
+  Future<void> setUserName(String name) async {
+    final prefs = ref.read(sharedPreferencesProvider);
+    await prefs.setString(kUserNameKey, name);
+    state = name;
+  }
+
+  Future<void> clearUserName() async {
+    final prefs = ref.read(sharedPreferencesProvider);
+    await prefs.remove(kUserNameKey);
+    state = '';
+  }
+
+}
