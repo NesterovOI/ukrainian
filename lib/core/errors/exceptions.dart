155e2dae116ca3ab5f6c4a7a64a41b61c3e907b5
NesterovOI <110261930+NesterovOI@users.noreply.github.com>
2026-07-31 14:41:31 +0300

    Created abstract class AuthException in core/errors/exceptions.dart

---
+++ lib/core/errors/exceptions.dart
@@ -0,0 +1,25 @@
+import 'package:ukrainian/core/theme/app_strings.dart';
+
+abstract class AuthException implements Exception {
+  final String message;
+
+  const AuthException(this.message);
+}
+
+class WeakPasswordAuthException extends AuthException {
+  const WeakPasswordAuthException() : super(AppStrings.weakPassword);
+}
+
+class EmailAlreadyInUseAuthException extends AuthException {
+  const EmailAlreadyInUseAuthException() : super(AppStrings.emailAlreadyInUse);
+}
+
+class InvalidEmailOrPasswordAuthException extends AuthException {
+  const InvalidEmailOrPasswordAuthException()
+    : super(AppStrings.invalidEmailOrPassword);
+}
+
+class GenericAuthException extends AuthException {
+  const GenericAuthException([String? message])
+    : super(message ?? AppStrings.invalidAuth);
+}
