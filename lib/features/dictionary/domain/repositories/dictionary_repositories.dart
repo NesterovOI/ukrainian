3c6e88d9e1bc3caf116bc11ec604fe1c9c367e58
NesterovOI <110261930+NesterovOI@users.noreply.github.com>
2026-08-02 15:44:19 +0300

    Added abstract class DictionaryRepositories in dictionary/domain/repositiries/ and create file rule.json in assets/data

---
+++ lib/features/dictionary/domain/repositories/dictionary_repositories.dart
@@ -0,0 +1,7 @@
+import 'package:ukrainian/features/dictionary/domain/entities/rule_entity.dart';
+
+abstract class DictionaryRepositories {
+  Future<List<RuleEntity>> getAllRules();
+  Future<RuleEntity> getRuleById(String id);
+  Future<List<RuleEntity>> getRulesByCategory(String categoryId);
+}
