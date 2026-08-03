24ba129df2ead7c2efa53ae9ba9b8320ad0cda67
NesterovOI <110261930+NesterovOI@users.noreply.github.com>
2026-08-03 17:49:31 +0300

    Aded function build() in DictionaryController

--- lib/features/dictionary/presentation/providers/dictionary_controller.dart
+++ lib/features/dictionary/presentation/providers/dictionary_controller.dart
@@ -1,14 +1,26 @@
 import 'dart:async';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:ukrainian/features/dictionary/domain/entities/rule_entity.dart';
+import 'package:ukrainian/features/dictionary/domain/usecases/search_rules_usecase.dart';
 import 'package:ukrainian/features/dictionary/presentation/providers/dictionary_providers.dart';
+
+final searchRulesUseCasesProvider = Provider<SearchRulesUseCase>((ref) {
+  final repo = ref.watch(dictionaryRepositoryProvider);
+  return SearchRulesUseCase(repo);
+});
 
 final selectedCategoryProvider = StateProvider<String>((ref) => 'all');
 
+final searchQueryProvider = StateProvider<String>((ref) => '');
+
 class DictionaryController extends AsyncNotifier<List<RuleEntity>> {
   @override
-  FutureOr<List<RuleEntity>> build() {
-    return _fetchRules();
+  Future<List<RuleEntity>> build() async {
+    final query = ref.watch(searchQueryProvider);
+    final category = ref.watch(selectedCategoryProvider);
+    final searchUseCase = ref.watch(searchRulesUseCasesProvider);
+
+    return searchUseCase(query: query, categoryId: category);
   }
 
   Future<List<RuleEntity>> _fetchRules() async {
