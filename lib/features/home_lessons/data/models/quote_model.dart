7d0c615df598873034c9d6e7a2b8e7d0affa155a
NesterovOI <110261930+NesterovOI@users.noreply.github.com>
2026-08-07 23:40:10 +0300

    Added file quote_model.dart

---
+++ lib/features/home_lessons/data/models/quote_model.dart
@@ -0,0 +1,25 @@
+import 'package:ukrainian/features/home_lessons/domain/entities/entities_home_export.dart';
+
+class QuoteModel extends QuoteEntity {
+  const QuoteModel({
+    required super.id,
+    required super.text,
+    required super.author,
+  });
+
+  factory QuoteModel.fromJson(Map<String, dynamic> json) {
+    return QuoteModel(
+      id: json['id'] as String ?? '',
+      text: json['text'] as String ?? '',
+      author: json['author'] as String ?? '',
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'text': text,
+      'author': author,
+    };
+  }
+}
