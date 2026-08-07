f19fa11f6643d5ce61b9f5c0e244fc7238be9754
NesterovOI <110261930+NesterovOI@users.noreply.github.com>
2026-08-07 22:11:11 +0300

    Added in home_lessons file quote_entity.dart

---
+++ lib/features/home_lessons/domain/entities/quote_entity.dart
@@ -0,0 +1,11 @@
+class QuoteEntity {
+  final String id;
+  final String text;
+  final String author;
+
+  const QuoteEntity({
+    required this.id,
+    required this.text,
+    required this.author,
+  });
+}
