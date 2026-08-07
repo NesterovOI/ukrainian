a387449e37dc4fc6e43d5f5897c5d43473f83d38
NesterovOI <110261930+NesterovOI@users.noreply.github.com>
2026-08-07 23:09:42 +0300

    Added file lesson_entity.dart

---
+++ lib/features/home_lessons/domain/entities/lesson_entity.dart
@@ -0,0 +1,21 @@
+import 'package:ukrainian/features/home_lessons/domain/entities/quiz_question_entity.dart';
+
+class LessonEntity {
+  final String id;
+  final String title;
+  final String description;
+  final int order;
+  final bool isFree;
+  final String theoryMarkdown;
+  final List<QuizQuestionEntity> question;
+
+  const LessonEntity({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.order,
+    required this.isFree,
+    required this.theoryMarkdown,
+    required this.question,
+  });
+}
