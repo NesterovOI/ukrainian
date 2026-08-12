0ded5cc81d8e33975353b179e3c962ade449014f
NesterovOI <110261930+NesterovOI@users.noreply.github.com>
2026-08-12 16:58:19 +0300

    Functional expansion in quiz_question_entity

--- lib/features/home_lessons/domain/entities/quiz_question_entity.dart
+++ lib/features/home_lessons/domain/entities/quiz_question_entity.dart
@@ -1,15 +1,29 @@
+enum QuestionType {
+  multipleChoice,
+  findError,
+  matching,
+}
+
 class QuizQuestionEntity {
   final String id;
   final String question;
+  final QuestionType type;
   final List<String> options;
-  final int correctOptionIndex;
+  final int? correctOptionIndex;
+  final List<String>? leftItems;
+  final List<String>? rightItems;
+  final Map<int, int>? correctPairis;
   final String explanation;
 
   const QuizQuestionEntity({
     required this.id,
     required this.question,
-    required this.options,
-    required this.correctOptionIndex,
+    this.type = QuestionType.multipleChoice,
+    this.options = const [],
+    this.correctOptionIndex,
+    this.leftItems,
+    this.rightItems,
+    this.correctPairis,
     required this.explanation,
   });
 }
