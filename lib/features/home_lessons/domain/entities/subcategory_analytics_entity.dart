9111121cb2694b84992488f7b1c965761b660a4d
NesterovOI <110261930+NesterovOI@users.noreply.github.com>
2026-08-21 23:58:49 +0300

    Created file SubcategoryAnalyticsEntity

---
+++ lib/features/home_lessons/domain/entities/subcategory_analytics_entity.dart
@@ -0,0 +1,11 @@
+class SubcategoryAnalyticsEntity {
+  final String subcategory;
+  final int totalAttems;
+  final double accuratePercentage;
+
+  SubcategoryAnalyticsEntity({
+    required this.subcategory,
+    required this.totalAttems,
+    required this.accuratePercentage,
+  });
+}
