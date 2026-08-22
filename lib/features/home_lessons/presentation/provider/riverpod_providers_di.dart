120fded400f4a10fc90b57eb5ef9b0d267382696
NesterovOI <110261930+NesterovOI@users.noreply.github.com>
2026-08-22 14:38:28 +0300

    Added new file in pages analytics_page.dart and modified riverpod_provider_di.dart new function analyticsFutureProvider

--- lib/features/home_lessons/presentation/provider/riverpod_providers_di.dart
+++ lib/features/home_lessons/presentation/provider/riverpod_providers_di.dart
@@ -2,6 +2,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:ukrainian/features/home_lessons/data/local/app_database.dart';
 import 'package:ukrainian/features/home_lessons/domain/repositories/user_progress_repository.dart';
 import 'package:ukrainian/features/home_lessons/data/repositories/user_progress_repository_impl.dart';
+import 'package:ukrainian/features/home_lessons/domain/entities/export_entities.dart';
 import 'package:ukrainian/features/home_lessons/domain/usecases/export_usecases.dart';
 
 final appDatabaseProvider = Provider<AppDatabase>((ref) {
@@ -16,6 +17,12 @@ final logAnswerUseCaseProvider = Provider<LogAnswerUseCase>((ref) {
   return LogAnswerUseCase(ref.watch(userProgressRepositoryProvider));
 });
 
-final getAnalyticsProvider = Provider<GetAnalyticsUseCase>((ref) {
+final getAnalyticsUseCaseProvider = Provider<GetAnalyticsUseCase>((ref) {
   return GetAnalyticsUseCase(ref.watch(userProgressRepositoryProvider));
 });
+
+final analyticsFutureProvider =
+    FutureProvider<List<SubcategoryAnalyticsEntity>>((ref) async {
+      final getAnalytics = ref.watch(getAnalyticsUseCaseProvider);
+      return await getAnalytics.call();
+    });
