12dde3a4e3869cd037864394b14a075ac801b833
NesterovOI <110261930+NesterovOI@users.noreply.github.com>
2026-09-03 20:30:03 +0300

    Added new file theme_provider for settings app

---
+++ lib/features/profile/presentation/providers/theme_provider.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class ThemeProvider extends StateNotifier<ThemeMode> {
+  ThemeProvider() : super(ThemeMode.light);
+
+  void toggleTheme() {
+    if (state == ThemeMode.light) {
+      state = ThemeMode.dark;
+    } else {
+      state = ThemeMode.light;
+    }
+  }
+}
+
+final themeProvider = StateNotifierProvider<ThemeProvider, ThemeMode>((ref) {
+  return ThemeProvider();
+});
