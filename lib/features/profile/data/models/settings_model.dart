71fffc8f108fd0ba8f72fa2919c503b814ea0192
NesterovOI <110261930+NesterovOI@users.noreply.github.com>
2026-09-03 22:30:05 +0300

    Added new file settings_model.dart

---
+++ lib/features/profile/data/models/settings_model.dart
@@ -0,0 +1,5 @@
+import 'package:ukrainian/features/profile/domain/entities/settings_entity.dart';
+
+class SettingsModel extends SettingsEntity {
+  SettingsModel({required super.theme, required super.push});
+}
