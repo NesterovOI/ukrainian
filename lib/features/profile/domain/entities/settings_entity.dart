c560fd8654e5056d2c34dc87327dfb7481c8368e
NesterovOI <110261930+NesterovOI@users.noreply.github.com>
2026-09-20 11:12:44 +0300

    Added two int: reminderHour, reminderMinute in settings_entity.dart

--- lib/features/profile/domain/entities/settings_entity.dart
+++ lib/features/profile/domain/entities/settings_entity.dart
@@ -1,10 +1,27 @@
 class SettingsEntity {
   final bool theme;
   final bool push;
+  final int reminderHour;
+  final int reminderMinute;
 
-  SettingsEntity({required this.theme, required this.push});
+  SettingsEntity({
+    required this.theme,
+    required this.push,
+    required this.reminderHour,
+    required this.reminderMinute,
+  });
 
-  SettingsEntity copyWith({bool? theme, bool? push}) {
-    return SettingsEntity(theme: theme ?? this.theme, push: push ?? this.push);
+  SettingsEntity copyWith({
+    bool? theme,
+    bool? push,
+    int? reminderHour,
+    int? reminderMinute,
+  }) {
+    return SettingsEntity(
+      theme: theme ?? this.theme,
+      push: push ?? this.push,
+      reminderHour: reminderHour ?? this.reminderHour,
+      reminderMinute: reminderMinute ?? this.reminderMinute,
+    );
   }
 }
